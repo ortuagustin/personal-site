4d6a4b82fbea486e995a12f6d1088b409523ca60
Agustín Ortu <ortu.agusitn@gmail.com>
2020-07-15 23:58:31 -0300

    add tailwind with simple boilerplate

---
+++ pages/about.md
@@ -0,0 +1,3 @@
+## About me
+
+I'm a cool guy from the Mars, nice to meet you folks.
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,19 @@
 <template>
-  <div>
-    <h1>My Blog</h1>
-    <p>This is my lovely (not yet) homepage!</p>
+  <div class="flex flex-col h-screen antialiased bg-gray-50 ">
+    <div class="container py-5 mx-auto space-y-5 divide-y-2">
+      <Navbar />
+
+      <div class="px-10 py-5">
+        <h1 class="text-3xl text-gray-900">Blog</h1>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+import Navbar from '../components/Navbar.vue'
+
+export default {
+  components: { Navbar },
+}
+</script>
