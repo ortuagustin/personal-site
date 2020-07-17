3a07f3f4ff12f84e9fd50ed8b2fb1c7bc6ff57a2
Agustín Ortu <ortu.agusitn@gmail.com>
2020-07-17 00:11:53 -0300

    add svg to vue loader

---
+++ src/saber-node.js
@@ -0,0 +1,18 @@
+exports.chainWebpack = function(config) {
+  // Only convert .svg files that are imported by these files as Vue component
+  const FILE_RE = /\.(vue|js|ts|svg)$/
+
+  // Use vue-cli's default rule for svg in non .vue .js .ts files
+  config.module.rule('svg').issuer(file => !FILE_RE.test(file))
+
+  // Use our loader to handle svg imported by other files
+  config.module
+    .rule('svg-component')
+    .test(/\.svg$/)
+    .issuer(file => FILE_RE.test(file))
+    .use('vue')
+    .loader('vue-loader')
+    .end()
+    .use('svg-to-vue-component')
+    .loader('svg-to-vue-component/loader')
+}
